c983caadc0b827c080ec304b0a64f2118470159c
C060291M <cmunoz6291@gmail.com>
2026-07-22 19:03:34 -0500

    feat: add Nacogdoches to BELL_PLATFORM_COUNTIES (esearch.nacocad.org, same platform). Verified live: 80/100 enriched at scale, clean run.

--- scraper/enrich_bell_rockwall.py
+++ scraper/enrich_bell_rockwall.py
@@ -29,6 +29,8 @@ COUNTIES = {
     "hockley":   "https://esearch.hockleycad.org",
     "jefferson": "https://esearch.jcad.org",
     "wilson":    "https://esearch.wilson-cad.org",
+    "galveston": "https://esearch.galvestoncad.org",
+    "nacogdoches": "https://esearch.nacocad.org",
     "hidalgo":   "https://hidalgo.prodigycad.com",
     "potter":    "https://www.prad.org",
     "randall":   "https://www.prad.org",
@@ -41,7 +43,7 @@ ROCKWALL_PLATFORM_COUNTIES = {"rockwall", "potter", "randall", "travis", "hidalg
 
 # Counties sharing the same underlying vendor platform as Bell (structured
 # OwnerName:X Year:Y query, #keywords field, Search() JS function)
-BELL_PLATFORM_COUNTIES = {"bell", "fort bend", "hunt", "kendall", "walker", "medina", "starr", "bee", "gillespie", "hockley", "jefferson", "wilson"}
+BELL_PLATFORM_COUNTIES = {"bell", "fort bend", "hunt", "kendall", "walker", "medina", "starr", "bee", "gillespie", "hockley", "jefferson", "wilson", "galveston", "nacogdoches"}
 
 def get_conn():
     return psycopg2.connect(DB, connect_timeout=30)
